bf278f9ea424e394079e93e67dee9b633dfe4ec3
“ShuzhongChen” <chenshuzhongs@gmail.com>
2017-10-09 17:47:18 +0000

    homepage jQuery UI: dragdrop function modification

--- public/js/dragdrop.js
+++ public/js/dragdrop.js
@@ -1,29 +1,27 @@
 $(init);
+var set = new Set();
 
 function init()
 {
-    cloneDragMe();
+    
 
-    $(".dragMe").draggable();
+    $(".science").draggable();
+    $(".sport").draggable();
+    $(".bussiness").draggable();
+    $(".technique").draggable();
+    
+    $(".science").css("top", 10);
+    $(".sport").css("top", 10);
+    $(".bussiness").css("top", 10);
+    $(".technique").css("top", 10);
+    
     $("#target").droppable();
 
     $("#target").bind("drop",    highlightTarget);
+    
     $("#target").bind("dropout", resetTarget);
     
 
-}
-
-function cloneDragMe()
-{
-    $var = $(".dragMe");
-    for (i = 1; i <= 4; i++){
-        xPos = 100*i + "px";
-        $var = $var.clone()
-                      .insertAfter($var)
-                      .css("left", xPos)
-                      .append("*");
-    }
-    
 }
 
 function highlightTarget(event, ui)
@@ -31,10 +29,34 @@ function highlightTarget(event, ui)
     $("#target").addClass("ui-state-highlight")
                 .html("Dropped ")
                 .append(ui.draggable.text());
+    set.add(ui.draggable.text());
+    printout();
 } 
 
 function resetTarget(event, ui)
 {
     $("#target").removeClass("ui-state-highlight")
-                .html("Drop on me");
+                .html("Drop your interest on me");
+    set.delete(ui.draggable.text());
+    printout();
 }
+
+
+function printout()
+{
+    var setIter = set.values();
+    var text = "Your interests: ";
+
+
+    for (i = 0; i < set.size; i++) {
+        if (i == set.size - 1) {
+            text += setIter.next().value;
+        } else {
+            text += setIter.next().value + ", ";
+        }
+        
+    }
+    document.getElementById("title").innerHTML = text;
+}
+
+
